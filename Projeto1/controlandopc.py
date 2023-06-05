620b7ddc5f3e8193e8b0484bd7caf76e8a9aa6dc
Rodrigo Emanuel Rabello <rodrigorabello@outlook.com>
2023-06-04 21:45:20 -0300

    Initial commit

---
+++ Projeto1/controlandopc.py
@@ -0,0 +1,7 @@
+import pyautogui
+pyautogui.PAUSE = 1
+pyautogui.press('winleft')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write('proporengenharia.com.br')
+pyautogui.press('enter')
---
+++ Projeto1/teste002.py
@@ -0,0 +1,3 @@
+#teste para abrir arquivos
+arquivo = open('meu arquivo.txt', 'w')
+arquivo.write('Olá mundo!')
